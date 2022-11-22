84b6ab739e9887260d607dc92ca826cc556fb81c
Bapt3175 <bapt.pustoch@gmail.com>
2022-11-22 13:57:17 +0100

    commit dapp

--- client/src/components/Demo/index.jsx
+++ client/src/components/Demo/index.jsx
@@ -3,29 +3,30 @@ import useEth from "../../contexts/EthContext/useEth";
 import Title from "./Title";
 import Cta from "./Cta";
 import Contract from "./Contract";
-import ContractBtns from "./ContractBtns";
+import Admin from "./Admin";
+import Voters from "./Voters";
 import Desc from "./Desc";
 import NoticeNoArtifact from "./NoticeNoArtifact";
 import NoticeWrongNetwork from "./NoticeWrongNetwork";
 
 function Demo() {
   const { state } = useEth();
-  const [value, setValue] = useState("?");
-  const [text, setText] = useState("init");
+  const [winner, setWinner] = useState("");
+  const [session, setSession] = useState("RegisteringVoters");
+  const [visibleOwner, setVisibleOwner] = useState(true); 
+  const [visibleVoter, setVisibleVoter] = useState(true); 
 
   const demo =
     <>
-      <Cta />
       <div className="contract-container">
-        <Contract value={value} text={text}/>
-        <ContractBtns setValue={setValue} setText={setText}/>
+        <Contract session={session} winner={winner}/>
+        <Voters  setVisibleOwner={setVisibleOwner} setVisibleVoter={setVisibleVoter} visibleVoter={visibleVoter} />
+        <Admin   setSession={setSession} setWinner={setWinner} visibleOwner={visibleOwner}/>
       </div>
-      <Desc />
     </>;
 
   return (
     <div className="demo">
-      <Title />
       {
         !state.artifact ? <NoticeNoArtifact /> :
           !state.contract ? <NoticeWrongNetwork /> :
--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -23,7 +23,7 @@ module.exports = {
   // Ajouter cette partie (ou décommenter directement dans le fichier):
   compilers: {
     solc: {
-      version: "0.8.14", // Récupérer la version exacte de solc-bin (par défaut : la  version de truffle)
+      version: "0.8.17", // Récupérer la version exacte de solc-bin (par défaut : la  version de truffle)
       settings: {  // Voir les documents de solidity pour des conseils sur l'optimisation et l'evmVersion
         optimizer: {
         enabled: false,
